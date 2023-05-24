0c811165471fb83c8b8efbd631ae97eb23e6be0a
Jacob Welin <43565798+jaco2716@users.noreply.github.com>
2023-05-24 19:51:24 +0200

    Elevation rotate

--- lib/constants/env_config.dart
+++ lib/constants/env_config.dart
@@ -2,8 +2,7 @@ import '../model/settings/route_config.dart';
 
 const List<RouteConfig> allRoutes = [
   RouteConfig(0, 'Camino Frances', 779.59),
-  RouteConfig(1, 'Camino New', 204.2),
-  RouteConfig(2, 'Camino Other', 402.5),
+  RouteConfig(1, 'Camino Test', 204.2),
 ];
 
 const String routeFilePrefix = 'route_file_';
--- lib/pages/elevation_chart_page.dart
+++ lib/pages/elevation_chart_page.dart
@@ -1,86 +1,134 @@
 import 'dart:math';
 
 import 'package:camino_nomad/model/providers/app_data_provider.dart';
+import 'package:camino_nomad/model/route_info/route_data.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
-class ElevationChartPage extends StatelessWidget {
+class ElevationChartPage extends StatefulWidget {
   const ElevationChartPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    var appDataP = context.read<AppDataProvider>();
-    var routeData = appDataP.currentRouteData!;
-    var cityRPIds = routeData.cities.map((e) => e.routePointId).toList();
-    int startEleIndex = routeData.routePoints.indexWhere((element) => element.id == cityRPIds[appDataP.startCityIndex]);
-    int endEleIndex = routeData.routePoints.indexWhere((element) => element.id == cityRPIds[appDataP.endCityIndex!]);
-    var eleMaxList = appDataP.allMaxEle?.getRange(appDataP.startCityIndex + 1, appDataP.endCityIndex! + 1);
-    double eleMax = eleMaxList?.reduce(max) ?? 0;
+  State<ElevationChartPage> createState() => _ElevationChartPageState();
+}
+
+class _ElevationChartPageState extends State<ElevationChartPage> {
+  bool isPortraitMode = true;
+  late AppDataProvider appDataP;
+  late RouteData routeData;
+  List<int> cityRPIds = [];
+  int startEleIndex = 0;
+  int endEleIndex = 0;
+  Iterable<double>? eleMaxList = [];
+  double eleMax = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    appDataP = context.read<AppDataProvider>();
+    routeData = appDataP.currentRouteData!;
+    cityRPIds = routeData.cities.map((e) => e.routePointId).toList();
+    startEleIndex = routeData.routePoints.indexWhere((element) => element.id == cityRPIds[appDataP.startCityIndex]);
+    endEleIndex = routeData.routePoints.indexWhere((element) => element.id == cityRPIds[appDataP.endCityIndex!]);
+    eleMaxList = appDataP.allMaxEle?.getRange(appDataP.startCityIndex + 1, appDataP.endCityIndex! + 1);
+    eleMax = eleMaxList?.reduce(max) ?? 0;
+
     if (eleMax < 1500) eleMax = 1500;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // var appDataP = context.read<AppDataProvider>();
+    // var routeData = appDataP.currentRouteData!;
+    // var cityRPIds = routeData.cities.map((e) => e.routePointId).toList();
+    // int startEleIndex = routeData.routePoints.indexWhere((element) => element.id == cityRPIds[appDataP.startCityIndex]);
+    // int endEleIndex = routeData.routePoints.indexWhere((element) => element.id == cityRPIds[appDataP.endCityIndex!]);
+    // var eleMaxList = appDataP.allMaxEle?.getRange(appDataP.startCityIndex + 1, appDataP.endCityIndex! + 1);
+    // double eleMax = eleMaxList?.reduce(max) ?? 0;
+    // if (eleMax < 1500) eleMax = 1500;
 
     // appDataP.allDistances?.forEach((element) {
     //   print(element);
     // });
     // var distSum = appDataP.allDistances?.reduce((a, b) => a + b);
     // print(distSum);
+    eleMax = 1500;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Elevation'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                isPortraitMode = !isPortraitMode;
+              });
+            },
+            icon: const Icon(Icons.screen_rotation_rounded),
+          )
+        ],
       ),
       body: SafeArea(
         // bottom: false,
         child: LayoutBuilder(builder: (context, constraints) {
-          return ListView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 0),
-            scrollDirection: Axis.horizontal,
-            itemCount: endEleIndex - startEleIndex + 1,
-            itemBuilder: (context, index) {
-              int rpIndex = index + startEleIndex;
-              double chartHeight = routeData.routePoints[rpIndex].ele / eleMax * constraints.maxHeight;
+          return RotatedBox(
+            quarterTurns: isPortraitMode ? 0 : 1,
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 0),
+              scrollDirection: Axis.horizontal,
+              itemCount: endEleIndex - startEleIndex + 1,
+              itemBuilder: (context, index) {
+                int rpIndex = index + startEleIndex;
 
-              if (cityRPIds.contains(routeData.routePoints[rpIndex].id)) {
-                var cityIndex = cityRPIds.indexOf(routeData.routePoints[rpIndex].id);
-                double totalDistance = 0;
-
-                for (var i = appDataP.startCityIndex + 1; i <= cityIndex; i++) {
-                  totalDistance += appDataP.allDistances?[i] ?? 0;
+                double chartHeight = routeData.routePoints[rpIndex].ele / eleMax * constraints.maxHeight;
+                if (!isPortraitMode) {
+                  chartHeight = routeData.routePoints[rpIndex].ele / eleMax * constraints.maxWidth;
                 }
 
-                return Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minHeight: chartHeight,
-                    ),
-                    child: Container(
-                      color: Colors.blue[600],
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 2),
-                        child: RotatedBox(
-                          quarterTurns: -1,
-                          child: Text(
-                            ' ${routeData.cities[cityIndex].name} - ${totalDistance.toStringAsFixed(2)} km',
-                            style: const TextStyle(
-                              height: 1,
-                              // fontSize: 14,
-                              color: Colors.white,
+                if (cityRPIds.contains(routeData.routePoints[rpIndex].id)) {
+                  var cityIndex = cityRPIds.indexOf(routeData.routePoints[rpIndex].id);
+                  double totalDistance = 0;
+
+                  for (var i = appDataP.startCityIndex + 1; i <= cityIndex; i++) {
+                    totalDistance += appDataP.allDistances?[i] ?? 0;
+                  }
+
+                  return Align(
+                    alignment: Alignment.bottomCenter,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        minHeight: chartHeight,
+                      ),
+                      child: Container(
+                        color: Colors.blue[600],
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 2),
+                          child: RotatedBox(
+                            quarterTurns: -1,
+                            child: Text(
+                              ' ${routeData.cities[cityIndex].name} - ${totalDistance.toStringAsFixed(2)} km',
+                              style: const TextStyle(
+                                height: 1,
+                                // fontSize: 14,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
+                  );
+                }
+                return Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: isPortraitMode ? 2 : 1,
+                    height: chartHeight,
+                    color: Colors.blue,
                   ),
                 );
-              }
-              return Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  width: 2,
-                  height: chartHeight,
-                  color: Colors.blue,
-                ),
-              );
-            },
+              },
+            ),
           );
         }),
       ),
